89bef8b8a914ed4b923f2e69c51c3396a8d5c1ce
Alexandra Marin <alexandra.marin01@gmail.com>
2020-07-01 17:55:54 +0300

    test: display inputs and result

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,18 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders inputs and results', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const amountElement = getByText(/Amount/i);
+  expect(amountElement).toBeInTheDocument();
+
+  const durationElement = getByText(/Duration/i);
+  expect(durationElement).toBeInTheDocument();
+
+  const interestElement = getByText(/interest rate/i);
+  expect(interestElement).toBeInTheDocument();
+
+  const paymentElement = getByText(/monthly payment/i);
+  expect(paymentElement).toBeInTheDocument();
 });
