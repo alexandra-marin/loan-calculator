a293b3d33e96692af75ac79d9b800d5c79dea7ff
Alexandra Marin <alexandra.marin01@gmail.com>
2020-07-01 17:17:05 +0300

    feat: fetch data and debounce call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import debounce from 'lodash.debounce';
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
   const minAmount = 1000;
   const maxAmount = 5000;
-
 
   const minDuration = 6;
   const maxDuration = 36;
@@ -16,6 +16,19 @@ const App = () => {
   const handleAmountChange = (e) => { setAmount(e.target.value) };
   const handleDurationChange = (e) => { setDuration(e.target.value) };
 
+  const fetchData = async () => {
+    console.log('>>> Requesting new estimate');
+    const response = await fetch("https://api.koyoloans.com/interest?amount=1000&numMonths=6");
+    console.log('>>>', await response.json());
+  };
+
+  const debounceOnChange = React.useCallback(debounce(fetchData, 400), []);
+
+  useEffect(() => {
+    debounceOnChange();
+  }, [amount, duration, debounceOnChange]);
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -25,11 +38,11 @@ const App = () => {
         </p>
       </header>
       <div>
-        <label for="amount">Amount</label>
+        <label htmlFor="amount">Amount</label>
         <input id="amount" type="range" min={minAmount} max={maxAmount} value={amount} onChange={handleAmountChange} />
         <p>{amount}</p>
 
-        <label for="duration">Duration</label>
+        <label htmlFor="duration">Duration</label>
         <input id="duration" type="range" min={minDuration} max={maxDuration} value={duration} onChange={handleDurationChange} />
         <p>{duration}</p>
 
