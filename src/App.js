ef8c89319b0246b38d14999ea72cfb15a3b15a49
Alexandra Marin <alexandra.marin01@gmail.com>
2020-07-01 16:21:41 +0300

    chore: add slider and label

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
-  const handleOnChange = () => { };
+  const handleAmountChange = () => { };
+  const handleDurationChange = () => { };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,7 +14,13 @@ const App = () => {
           Loan Calculator
         </p>
       </header>
+      <div>
+        <label for="amount">Amount</label>
+        <input id="amount" type="range" min="1" max="100" value="50" onChange={handleAmountChange} />
 
+        <label for="duration">Duration</label>
+        <input id="duration" type="range" min="1" max="100" value="50" onChange={handleDurationChange} />
+      </div>
     </div>
   );
 };
